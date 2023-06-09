9a221a271867e9b96ac5067fe9e66dccd0efbb67
atalyanta <atalyanta@gmail.com>
2023-06-09 12:14:08 +0300

    commit11

---
+++ home_work/3_hw.py
@@ -0,0 +1,44 @@
+                                #2
+a=5
+b=4
+if a>b:
+    print(a)
+elif a<b:
+    print(b)
+else:
+    print('Числа равны')
+
+                                #3
+a=1
+b=134
+if a-b==135 or b-a==135:
+    print('yes')
+else:
+    print('No')
+
+                                #4
+month=20
+if month in range(3,6):
+    print('весна')
+elif month in range(6,9):
+    print('лето')
+elif month in range(9,12):
+    print('осень')
+elif month in range(1-3) or 12:
+    print('зима')
+else:
+    print('Введите корректный номер месяца')
+
+                                #5
+a=15
+b=12
+c=11
+if a>10 and b>10 and c>10:
+    print('yes')
+else:
+    print('no')
+
+
+
+
+
